2b1de9a44c7cf13f677ff00907f9270db3eaf2f2
LHYPentest <>
2025-11-27 10:15:14 +0000

    String Indexing

---
+++ variables_and_types/string_indexing/main.py
@@ -0,0 +1,18 @@
+grocery_item = "Grilled Chicken Salad"
+lenght_of_item = grocery_item
+
+#positive indexing
+first_char = grocery_item[0]
+second_char = grocery_item[1]
+third_char = grocery_item[2]
+
+#negative indexing
+last_char1 =
+last_char2
+last_cahr3
+
+
+# Testing
+print("Length of item name:", length_of_item)
+print("First character of each word:", first_char, second_char, third_char)
+print("Last character of each word:", last_char1, last_char2, last_char3)
